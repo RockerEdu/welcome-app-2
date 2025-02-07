d5d61daab53d0d3c55700547db01c94977040886
RockerEdu <rocker_edu@hotmail.com>
2025-02-07 00:06:39 -0600

    Add files via upload

---
+++ Welcome-2/Welcome-2/Welcome_2App.swift
@@ -0,0 +1,19 @@
+//
+//  Welcome_2App.swift
+//  Welcome-2
+//
+//  Created by Eduardo Navarro on 06/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Welcome_2App: App {
+    let str = "aws_access_key_id = 'AKIAVFIWIPTQZMW7IFV3"
+    let str = "aws_secret_access_key = '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"
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
